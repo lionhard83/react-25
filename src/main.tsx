25946e04db1630b7ce11a55d8b1a5daa8483da4b
lionhard83 <carloleonardi83@gmail.com>
2025-04-29 18:48:06 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ExampleWithArray } from './ExampleWithArray.tsx'
+// import './index.css'
+
+
+// import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(<ExampleWithArray />)
